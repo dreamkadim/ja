a0e801173b40d6695b67acb7c0ee842f88c7830e
Michał Masiulanis <m.masiulanis@gmail.com>
2023-01-07 00:16:58 +0100

    zad 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerID = null;
+
+// function startChangingColors() {
+//   const randomColor = getRandomHexColor();
+//   body.style.backgroundColor = randomColor;
+//   console.log(`Background color is: ${randomColor}`);
+// }
+
+startBtn.addEventListener('click', () => {
+  timerID = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+    console.log(`Background color is: ${randomColor}`);
+    startBtn.disabled = true;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerID);
+  console.log('Changing color has stopped.');
+  startBtn.disabled = false;
+});
