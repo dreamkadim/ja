b875c77eac73a98afc989567bd60c2b1dd10503c
michamas <65348989+michamas@users.noreply.github.com>
2023-01-06 23:17:24 +0000

    Deploying to gh-pages from @ michamas/goit-js-hw-09@a0e801173b40d6695b67acb7c0ee842f88c7830e 🚀

---
+++ 01-color-switcher.d934771f.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]"),e=document.querySelector("body");let l=null;o.addEventListener("click",(()=>{l=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;e.style.backgroundColor=t,console.log(`Background color is: ${t}`),o.disabled=!0}),1e3)})),t.addEventListener("click",(()=>{clearInterval(l),console.log("Changing color has stopped."),o.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.d934771f.js.map
