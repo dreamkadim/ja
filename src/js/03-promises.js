83be97b64b6a88a23e7d7007063eb8860a7c47e8
Michał Masiulanis <m.masiulanis@gmail.com>
2023-01-15 22:36:50 +0100

    3 for check

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,22 +2,31 @@ import Notiflix from 'notiflix';
 
 const firstDelay = parseInt(document.querySelector('input[name=delay]').value);
 const delay = parseInt(document.querySelector('input[name=step]').value);
-const amountInput = parseInt(
-  document.querySelector('input[name=amount]').value
-);
+const amount = parseInt(document.querySelector('input[name=amount]').value);
 
 const submitBtn = document.querySelector('button[type=submit]');
 
+// event watching submitBtn and calling function createPromise "amount" times
 submitBtn.addEventListener('click', e => {
   e.preventDefault();
   setTimeout(() => {
-    for (let i = 1; i <= amountInput; i++) {
-      console.log(i);
-      // return createPromise;
+    for (let position = 1; position <= amount; position++) {
+      console.log(position);
+      return createPromise();
     }
   }, firstDelay);
 });
 
+// // test event - comment Ln9-18
+// submitBtn.addEventListener('click', e => {
+//   e.preventDefault();
+//   for (let position = 1; position <= amount; position++) {
+//     console.log(position);
+//   }
+// });
+
+// function creating promise with random result, with a delay that user put in' delay'
+// no matter the result promise gives object {position, delay}
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -33,6 +42,7 @@ function createPromise(position, delay) {
   });
 }
 
+// what happens when promise is fulfilled and what when rejected
 createPromise()
   .then(({ position, delay }) => {
     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
